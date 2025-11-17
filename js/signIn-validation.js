3b95dc39355a511979f034432ec76dd9839043ad
hadil-belhadri <hadil.belhadri@ensia.edu.dz>
2025-11-17 01:32:32 +0100

    first commit

---
+++ js/signIn-validation.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    //sign in validation 
+
+    let EmailIn = document.querySelector("#email");
+    let PasswordIn = document.querySelector("#password");
+    let Result = document.querySelector("#RESULT");
+    let SignInBTN = document.querySelector("#SIGNIN");
+
+
+    SignInBTN.addEventListener("click", (event) => {
+        event.preventDefault();
+        CheckInputs();
+    });
+    function setErrorFor(input, message) {
+        Result.innerText += "\n" + message;
+        input.classList.add("error");
+        input.classList.remove("success");
+    }
+
+    function setSuccessFor(input) {
+        input.classList.add("success");
+        input.classList.remove("error");
+    }
+    function CheckInputs() {
+
+        const EmailSIGNIN = EmailIn.value.trim();
+        const Pass = PasswordIn.value.trim();
+       const passwordRegex = /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8}$/;
+
+        const mail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+        Result.innerText = "";
+
+        if (!EmailSIGNIN || !mail.test(EmailSIGNIN)) {
+            setErrorFor(EmailIn, "Invalid email");
+            return;
+        } else {
+            setSuccessFor(EmailIn);
+        }
+        if (!Pass || !passwordRegex.test(Pass)) {
+            setErrorFor(PasswordIn, "Password must be 8 chars and include a letter, number, and one of: @$!%*?&");
+            return;
+        } else {
+            setSuccessFor(PasswordIn);
+        }
+
+    }
+
+});
