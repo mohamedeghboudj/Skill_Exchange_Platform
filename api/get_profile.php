9edb68ab63f325391a87276c353bb7f0c359a4f9
hadil-belhadri <hadil.belhadri@ensia.edu.dz>
2026-02-03 03:07:50 +0100

    :

--- api/get_profile.php
+++ api/get_profile.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1); // disabling error reporting 
 
 session_start();
-require_once '/config/db.php';
+require_once '../config/db.php';
 
 if (ob_get_level()) ob_end_clean();
 
