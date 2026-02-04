0e9675d061ecf2e162ac92d05631a06d02645757
ikramhenniene <ikram.henniene@ensia.edu.dz>
2026-02-04 20:52:27 +0000

    progress edit

---
+++ assets/js/videoPlayer.js
@@ -0,0 +1,7 @@
+const crossIcon = document.getElementById("cross");
+
+
+crossIcon.addEventListener("click", () => {
+    // Redirect to courseinfo page
+    window.location.href = "/pages/courseInfo.html"; 
+});
